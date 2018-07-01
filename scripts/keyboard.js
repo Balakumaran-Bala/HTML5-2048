659f1512ed9793ac11de2323815be3562cb17e21
keanemind <keanemind@gmail.com>
2018-06-30 20:54:16 -0500

    Add keyboard event listeners

---
+++ scripts/keyboard.js
@@ -0,0 +1,18 @@
+var keyDown = false;
+
+var keyboardInit = function() {
+    addEventListener(keydown, function(e) {
+         // only call update once
+        if (!keyDown && [37, 38, 39, 40].includes(e.keyCode)) {
+            keyDown = true;
+            update(e.keyCode);
+            render();
+        }
+    }, false);
+
+    addEventListener(keyup, function(e) {
+        if ([37, 38, 39, 40].includes(e.keyCode)) {
+            keyDown = false;
+        }
+    })
+}
