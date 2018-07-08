4a55dfbbf6fdee61148cf1906f79fe4f2999de69
keanemind <keanemind@gmail.com>
2018-07-07 21:38:00 -0500

    Prevent arrow key scrolling

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,6 +1,7 @@
 var keyboardInit = function() {
     addEventListener("keydown", function(e) {
         if ([37, 38, 39, 40].includes(e.keyCode)) {
+            e.preventDefault();
             update(e.keyCode);
         }
     }, false);
